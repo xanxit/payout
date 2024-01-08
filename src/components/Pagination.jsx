2b99ab1abe1a3987ae369b7e7a46cf60274aed13
Sanchit Hajela <sanchit10hajela@gmail.com>
2024-01-09 01:10:00 +0530

    fix: pagination for mobile

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,9 +1,9 @@
 const Pagination = () => {
   const numbers = Array.from({ length: 11 }, (_, index) => index + 10);
   return (
-    <div className="">
-      <nav className="my-16 md:my-3">
-        <ul className="flex flex-wrap items-center md:gap-x-2 text-base h-10">
+    <div className="px-8 lg:px-0">
+      <nav className="my-16 md:my-4">
+        <ul className="flex px-10 lg:px-0 items-center md:gap-x-2 text-xs md:text-base h-10">
           <li>
             <a
               href="#"
@@ -30,11 +30,11 @@ const Pagination = () => {
             </a>
           </li>
           {numbers.map((number, index) => (
-            <li key={index}>
+            <li className={`${index>2 && 'hidden lg:block'}`} key={index}>
               <a
                 href="#"
                 aria-current="page"
-                className={`flex items-center justify-center px-4 h-8 w-auto rounded-lg ${number===10 ? 'text-white bg-[#146EB4] hover:bg-[#146fb4ba] hover:text-white' : 'text-gray-500 bg-white hover:bg-gray-100 hover:text-gray-700'}`}
+                className={`flex items-center justify-center px-2 md:px-4 h-8 w-auto rounded-lg ${number===10 ? 'text-white bg-[#146EB4] hover:bg-[#146fb4ba] hover:text-white' : 'text-gray-500 bg-white hover:bg-gray-100 hover:text-gray-700'}`}
               >
                 {number}
               </a>
